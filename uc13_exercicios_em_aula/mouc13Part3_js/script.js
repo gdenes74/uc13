49f0e26fd26b16f5a95c82e8eeeed2fd584b609e
Guilherme Denes <gui.italia2015@gmail.com>
2023-12-31 14:48:27 -0300

    Inclusão de uma pasta de exercícios resolvidos em aula, atualizando uc 13

---
+++ uc13_exercicios_em_aula/mouc13Part3_js/script.js
@@ -0,0 +1,35 @@
+function mostrarDados(){
+    alert("mostrando dados ao clicar no botão Acessar");
+}
+let variavel = " MO uc13 "
+alert (variavel);
+
+//operadores : ADICAO
+let resultado = 10 + 20;
+alert (resultado);
+
+//operador: subtração 
+let resultado1 = 33-18;
+alert (resultado1);
+
+//operador multiplicação
+let resultado2 =10*22;
+alert (resultado2);
+
+//operador divisão
+let resultado3 =40/10;
+alert (resultado3);
+
+// operação Resto da Divisão
+let resultado4 = 29%3;
+alert (resultado4);
+
+//operação Exponencial
+let resultado5= 9**3;
+alert (resultado5);
+
+
+
+alert (" o nome da aula de hoje é: " + variavel);
+
+alert (" A multiplicação do resultado 2: é " + resultado2);  
